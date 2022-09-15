6db33f2f52e1b8a57b17443d4b1c048e0580923b
Raul Ribeiro <raulribeiro@192.168.1.4>
2022-09-15 16:13:23 -0300

    Atributos encapsulados

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -1,12 +1,40 @@
 package bytebank;
 
 public class Conta {
-	double saldo;
-    int agencia;
-    int numero;
-    String titular;
+	private double saldo;
+    private int agencia;
+    private int numero;
+	Cliente cliente;
     
-    public void deposita(double valor) {
+    public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void deposita(double valor) {
     	this.saldo += valor;
     }
     
@@ -20,6 +48,9 @@ public class Conta {
     
     public boolean transfere(double valor, Conta destino) {
     	if (this.saldo >= valor) {
+    		System.out.println("Transferindo R$" + valor + 
+    				" da conta de " + this.getCliente().getNome() + 
+    				" para a conta de " + destino.getCliente().getNome());
     		destino.deposita(this.saca(valor));
     		return true;
     	}
--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -2,13 +2,17 @@ package bytebank;
 
 public class TestaMetodo {
 	public static void main(String[] args) {
-		Conta raul = new Conta();
-		raul.deposita(100);
-		System.out.println(raul.saldo);
+		Conta contaDoRaul = new Conta();
+		contaDoRaul.setCliente(new Cliente());
+		contaDoRaul.getCliente().setNome("Raul");
+		contaDoRaul.deposita(100);
+		System.out.println("Saldo do Raul: R$" + contaDoRaul.getSaldo());
 		
-		Conta lu = new Conta();
-		raul.transfere(100, lu);
-		System.out.println(lu.saldo);
-		System.out.println(raul.saldo);
+		Conta contaDaLu = new Conta();
+		contaDaLu.setCliente(new Cliente());
+		contaDaLu.getCliente().setNome("Lu");
+		contaDoRaul.transfere(100, contaDaLu);
+		System.out.println("Saldo da Lu: R$" + contaDaLu.getSaldo());
+		System.out.println("Saldo do Raul: R$" + contaDoRaul.getSaldo());
 	}
 }
